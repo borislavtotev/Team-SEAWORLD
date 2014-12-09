990bbc4aa04c27848578740103f67475cc0c323f
asyamincheva <asya.m.mincheva@gmail.com>
2014-12-10 01:12:14 +0200

    Added comments about the plug-ins

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -4,16 +4,23 @@
     <div class="row">
         <div class="col-md-8">Copyright 2014 Programators</div>
         <div class="col-md-4">
+
+            <!--Google+ plug-in-->
             <div class="g-plusone" data-annotation="inline" data-width="300"></div>
+
+            <!--Facebook plug-in-->
             <div class="fb-like" data-href="https://developers.facebook.com/docs/plugins/"
                  data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
-        </div>
+            </div>
     </div>
 </footer>
 
+<!--Google+ plug-in script-->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 <script src="https://apis.google.com/js/platform.js" async defer></script>
-<script src="js/lib/bootstrap.min.js"></script>
+<!--Facebook plug-in script; used link to the file-->
 <script src="js/facebook.js"></script>
+<!--Bootstrap script; used link to the file-->
+<script src="js/lib/bootstrap.min.js"></script>
 </body>
 </html>
