f7149bd5de77c2ba404a93faf8bdfedd2712fb95
d0ntth1nc <d0ntth1nc@gmail.com>
2014-12-10 01:05:53 +0200

    lets not mess up the html footer fragment

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,14 +1,5 @@
 <body>
-<!--Facebook plug-in-->
 <div id="fb-root"></div>
-<script>(function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.0";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));</script>
-<!--End of Facebook plug-in-->
 <footer class="container">
     <div class="row">
         <div class="col-md-8">Copyright 2014 Programators</div>
@@ -23,24 +14,6 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 <script src="https://apis.google.com/js/platform.js" async defer></script>
 <script src="js/lib/bootstrap.min.js"></script>
-<!--Facebook script-->
-<script>
-    window.fbAsyncInit = function() {
-        FB.init({
-            appId      : 'your-app-id',
-            xfbml      : true,
-            version    : 'v2.1'
-        });
-    };
-
-    (function(d, s, id){
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) {return;}
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/en_US/sdk.js";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-</script>
-<!--End of Facebook script-->
+<script src="js/facebook.js"></script>
 </body>
 </html>
