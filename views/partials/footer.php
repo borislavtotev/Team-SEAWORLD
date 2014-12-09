d589d76b10ac8dbffef014f6ae4e0ce5af70fcdc
d0ntth1nc <d0ntth1nc@gmail.com>
2014-12-10 00:16:59 +0200

    added google plus button

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,3 +1,14 @@
+<footer class="container">
+    <div class="row">
+        <div class="col-md-8">Copyright 2014 Programators</div>
+        <div class="col-md-4">
+            <div class="g-plusone" data-annotation="inline" data-width="300"></div>
+        </div>
+    </div>
+</footer>
+
+
+<script src="https://apis.google.com/js/platform.js" async defer></script>
 <script src="js/lib/bootstrap.min.js"></script>
 </body>
 </html>
