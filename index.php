b677ec9db1cad7409718f534eb7931fe0314e4e7
d0ntth1nc <d0ntth1nc@gmail.com>
2014-12-11 11:07:10 +0200

    sample index page

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
-<?php include_once( 'views/partials/header.php' ); ?>
-<?php include_once('views/partials/navbar.php'); ?>
+<?php
+include_once( 'views/partials/header.php' );
+include_once('views/partials/navbar.php');
+/*
+ * Simulate extraction from db
+ */
+
+$albums = [
+    (object)[ 'name' => 'Nai-qkiq', 'owner' => 'Unknown' ],
+    (object)[ 'name' => 'Second album', 'owner' => 'Known' ]
+];
+$pictures = [
+    (object)[ 'title' => 'Porn pic', 'owner' => 'Unknown' ],
+    (object)[ 'title' => 'Second pic', 'owner' => 'Known' ]
+];
+
+?>
 <div class="container">
     <header class="row">
         <div class="col-md-12">
@@ -10,7 +25,48 @@
         </div>
     </header>
     <main class="row">
-
+        <div class="col-md-6">
+            <section class="panel panel-danger">
+                <header class="panel-heading">
+                    <h3 class="panel-title">Top rated albums</h3>
+                </header>
+                <div class="panel-body">
+                    <div class="list-group">
+                    <?php foreach($albums as $album): ?>
+                        <a href="#" class="list-group-item">
+                            <h4 class="list-group-item-heading">
+                                <?= htmlspecialchars( $album->name ) ?>
+                            </h4>
+                            <p class="list-group-item-text">
+                                Made by <?= htmlspecialchars( $album->owner ) ?>
+                            </p>
+                        </a>
+                    <?php endforeach; ?>
+                    </div>
+                </div>
+            </section>
+        </div>
+        <div class="col-md-6">
+            <section class="panel panel-info">
+                <header class="panel-heading">
+                    <h3 class="panel-title">Top rated pictures</h3>
+                </header>
+                <div class="panel-body">
+                    <div class="list-group">
+                    <?php foreach($pictures as $picture): ?>
+                        <a href="#" class="list-group-item">
+                            <h4 class="list-group-item-heading">
+                                <?= htmlspecialchars( $picture->title ) ?>
+                            </h4>
+                            <p class="list-group-item-text">
+                                Uploaded by <?= htmlspecialchars( $picture->owner ) ?>
+                            </p>
+                        </a>
+                    <?php endforeach; ?>
+                    </div>
+                </div>
+            </section>
+        </div>
     </main>
 </div>
 <?php include_once( 'views/partials/footer.php' ); ?>
