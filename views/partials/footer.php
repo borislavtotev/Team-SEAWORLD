46c28f46308a899fcdded337ccb65c1459b515b0
d0ntth1nc <d0ntth1nc@gmail.com>
2014-12-11 12:44:05 +0200

    improved footer

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,26 +1,34 @@
-<body>
 <div id="fb-root"></div>
 <footer class="container">
-    <div class="row">
-        <div class="col-md-8">Copyright 2014 Programators</div>
-        <div class="col-md-4">
+    <div class="row well">
+        <div class="col-md-6">
+            <p>Copyright 2014 Programators</p>
+        </div>
+        <div class="col-md-6">
+            <div class="row">
+                <!--Google+ plug-in container-->
+                <div class="col-md-12">
+                    <div class="g-plusone" data-annotation="inline" data-width="300"></div>
+                </div>
 
-            <!--Google+ plug-in-->
-            <div class="g-plusone" data-annotation="inline" data-width="300"></div>
-
-            <!--Facebook plug-in-->
-            <div class="fb-like" data-href="https://developers.facebook.com/docs/plugins/"
-                 data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
+                <!--Facebook plug-in container-->
+                <div class="col-md-12">
+                    <div class="fb-like" data-href="https://developers.facebook.com/docs/plugins/"
+                         data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
+                </div>
+            </div>
         </div>
     </div>
 </footer>
 
-<!--Google+ plug-in script-->
+<!--Google+ plug-in-->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 <script src="https://apis.google.com/js/platform.js" async defer></script>
-<!--Facebook plug-in script; used link to the file-->
+
+<!--Facebook plug-in-->
 <script src="js/facebook.js"></script>
-<!--Bootstrap script; used link to the file-->
+
+<!--Bootstrap-->
 <script src="js/lib/bootstrap.min.js"></script>
 </body>
 </html>
