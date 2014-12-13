96523f86c4bf5da54270a63b651bae4129046ef5
d0ntth1nc <d0ntth1nc@gmail.com>
2014-12-13 13:27:56 +0200

    user model improved

--- system/models/user.php
+++ system/models/user.php
@@ -1,72 +1,95 @@
 <?php
 class User
 {
-    private $mysqli;
-	private $username;
-	private $password;
+    private $userId;
+	private $userName;
 
-    public function __construct( $mysqli, $username, $password )
+    public function __construct( $userId, $userName )
     {
-        $this->mysqli = $mysqli;
-			
-        if (!$this->checkInputs($username, $password)) {
-            die ( 'Name and userID cannot be empty!' );
-        } 
-
-        $query = "INSERT INTO user(username, password) VALUES('$this->username', '$this->password')";
-
-        return mysqli_query( $this->mysqli, $query ) or die( mysqli_error( $this->mysqli ) );
+        $this->userId = $userId;
+        $this->userName = $userName;
     }
 
-    public static function checkUser($mysqli, $username, $password )
+    /*
+     * Static methods
+     */
+    public static function createUser( $mysqli, $userName, $password )
     {
-        if (empty( User::parseInput($username) ) || empty( User::parseInput($username) ) ) {
-            die ( 'Name and userID cannot be empty!' );
+        $escapedUserName = User::parseInput( $userName );
+        $password = md5( $password );
+
+        /*
+         * TODO: Check if user with this name already exists!
+         */
+        $query = "INSERT INTO user(username, password) VALUES('$escapedUserName', '$password')";
+        mysqli_query( $mysqli, $query ) or die( mysqli_error( $mysqli ) );
+
+        /*
+         * Get the user id from the database and create instance...
+         */
+        $query = "SELECT userid FROM user WHERE username='$escapedUserName' AND password='$password'";
+        $result = $mysqli->query( $query ) or die( mysqli_error( $mysqli ) );
+        $id = $result->fetch_assoc();
+
+        $password = null; // Delete that info from the memory!!
+        return new User( $id, $userName );
+    }
+
+    public static function isLoginDataValid( $mysqli, $username, $password )
+    {
+        $parsedUsrName = User::parseInput( $username );
+        $parsedPass = User::parseInput( $password );
+
+        if (empty( $parsedUsrName ) || empty( $parsedPass ) ) {
+            die ( 'Name and pass cannot be empty!' );
 		}	
 			
 		$query = "SELECT * FROM user WHERE username='$username' AND password='$password'";
-        $checkMatch = mysqli_query($mysqli,$query) or die( mysqli_error( $mysqli ));
-		echo var_dump($checkMatch);
+        $checkMatch = mysqli_query( $mysqli, $query ) or die( mysqli_error( $mysqli ) );
+
         if ($checkMatch->num_rows == 1) {
         	return true;
 		} else {
 			return false;
 		}
     }
-	
-	public function getUserId ()
-	{
-		$query = "SELECT userid FROM user WHERE username='$this->username' AND password='$this->password'";
-        $checkMatch = mysqli_query($this->mysqli,$query) or die( mysqli_error( $this->mysqli ));
-		echo var_dump($checkMatch);
-        if ($checkMatch->num_rows == 1) {
-        	while ($row=mysqli_fetch_assoc($checkMatch)){
-                echo var_dump($row);	
-                return $row['userid'];     
-            }
-		} else {
-			return false;
-		}					
-	}
 
-    private function parseInput( $input )
+    /*
+     * Getters
+     */
+    public function getId()
+    {
+        return $this->userId;
+    }
+
+    public function getUserName()
+    {
+        return $this->userName;
+    }
+
+    public function getAlbums( $mysqli )
+    {
+        include_once 'system/models/album.php';
+        return Album::getAlbumsByOwnerId( $mysqli, $this->userId );
+    }
+
+    /*
+     * Setters
+     */
+    public function setPassword( $mysqli, $password )
+    {
+        $password = md5( $password );
+        $query = "UPDATE user SET password='$password' WHERE userid='$this->userId'";
+        $password = null;
+        mysqli_query( $mysqli, $query ) or die( mysqli_error( $mysqli ) );
+    }
+
+    /*
+     * Private methods
+     */
+    private static function parseInput( $input )
     {
         $regex = '/[^a-zA-Z0-9_]/';
         return preg_replace( $regex, '', $input );
     }
-	
-	private function checkInputs( $username, $password )
-	{
-
-        if (empty( $this->parseInput($username) ) || empty( $this->parseInput($password) ) ) {
-            return false;
-        } else {
-        	$this->username = $this->parseInput($username);
-        	$this->password = $this->parseInput($password);
-        	return true;
-        }
-		
-	}
-
-
 }
