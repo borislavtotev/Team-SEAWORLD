cb87c26fdbd6a8e541744112c7b9d776ff5441c4
Bebbo <mrbebz@gmail.com>
2014-12-13 17:53:00 +0200

    Revert "tests upload"
    This reverts commit 38924b7e378e189cfa6bd9610df24f4abf288166.

--- image-upload.php
+++ image-upload.php
@@ -3,3 +3,8 @@
 <button type="button" class="btn btn-danger btn-lg" data-toggle="modal" data-target="#filesUpload">
     Upload files
 </button>
+<?php
+    if(isset ($_POST['files'])){
+        echo "true";
+    };
+?>
