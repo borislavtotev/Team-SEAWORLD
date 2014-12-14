dec5b1bec35abcc3f5b5ea19869be471408a6329
Bebbo <mrbebz@gmail.com>
2014-12-15 00:31:58 +0200

    upld-changes
    changes upload logic

--- image-upload.php
+++ image-upload.php
@@ -1,6 +1,6 @@
 <?php
-if((!isset($_SESSION['user']))) {
-    header("Location:index.php");
+    if((!isset($_SESSION['user']))) {
+        header("Location:index.php");
 }
 ?>
 <?php include 'views/modals/file-upload.php'; ?>
@@ -10,7 +10,7 @@ if((!isset($_SESSION['user']))) {
 </button>
 <?php
     $target_dir = "database/upload/";
-    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+    $target_file = $target_dir . basename($_FILES["files[]"]["name"]);
     $uploadOk = 1;
     $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
     // Check if image file is a actual image or fake image
@@ -24,23 +24,23 @@ if((!isset($_SESSION['user']))) {
             $uploadOk = 0;
         }
     }
-    // Check if file already exists
+    // Check if file exists...ebem go
     if (file_exists($target_file)) {
         echo "Sorry, file already exists.";
         $uploadOk = 0;
     }
-    // Check file size
-    if ($_FILES["fileToUpload"]["size"] > 500000) {
+    // Check file size(kolko e goem)
+    if ($_FILES["files[]"]["size"] > 500000) {
         echo "Sorry, your file is too large.";
         $uploadOk = 0;
     }
-    // Allow certain file formats
+    // Allow certain file formats(formati)
     if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
         && $imageFileType != "gif" ) {
         echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
         $uploadOk = 0;
     }
-    // Check if $uploadOk is set to 0 by an error
+    // Check if $uploadOk is set to 0 by an error(kyr demek)
     if ($uploadOk == 0) {
         echo "Sorry, your file was not uploaded.";
     // if everything is ok, try to upload file
