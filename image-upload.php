da54162f54bd0aa794b8f1a722e09e90d6f6e12e
Bebbo <mrbebz@gmail.com>
2014-12-14 23:21:54 +0200

    upld-script-
    check user log

--- image-upload.php
+++ image-upload.php
@@ -3,3 +3,9 @@
 <button type="button" class="btn btn-danger btn-lg" data-toggle="modal" data-target="#filesUpload">
     Upload files
 </button>
+<?php
+    if((!isset($_SESSION['user']))) {
+        header("Location:index.php?p=1");
+        exit();
+    }
+    ?>
