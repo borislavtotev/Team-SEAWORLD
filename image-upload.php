c639648280e074ba66fa493ffe4bf070c238c8c4
Bebbo <mrbebz@gmail.com>
2014-12-14 23:24:32 +0200

    fixed check

--- image-upload.php
+++ image-upload.php
@@ -1,11 +1,10 @@
+<?php
+if((!isset($_SESSION['user']))) {
+    header("Location:index.php");
+}
+?>
 <?php include 'views/modals/file-upload.php'; ?>
 
 <button type="button" class="btn btn-danger btn-lg" data-toggle="modal" data-target="#filesUpload">
     Upload files
 </button>
-<?php
-    if((!isset($_SESSION['user']))) {
-        header("Location:index.php?p=1");
-        exit();
-    }
-    ?>
