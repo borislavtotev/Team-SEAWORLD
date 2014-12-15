b87387a44f9c3efa882fbc8329a8c62cc8656770
Bebbo <mrbebz@gmail.com>
2014-12-15 20:41:32 +0200

    change names
    change names

--- image-upload.php
+++ image-upload.php
@@ -5,17 +5,17 @@
 ?>
 <?php include 'views/modals/file-upload.php'; ?>
 
-<button type="button" class="btn btn-danger btn-lg" data-toggle="modal" data-target="#filesUpload">
+<button type="button" class="btn btn-danger btn-lg" data-toggle="modal" data-target="#filesUpload" name="imgUpload">
     Upload files
 </button>
 <?php
     $target_dir = "upload/";
-    $target_file = $target_dir . basename($_FILES["files[]"]["name"]);
+    $target_file = $target_dir . basename($_FILES["imgUpload"]["name"]);
     $uploadOk = 1;
     $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
     // Check if image file is a actual image or fake image
-    if(isset($_POST["submit"])) {
-        $check = getimagesize($_FILES["files[]"]["tmp_name"]);
+    if(isset($_POST["imgUpload"])) {
+        $check = getimagesize($_FILES["imgUpload"]["tmp_name"]);
         if($check !== false) {
             echo "File is an image - " . $check["mime"] . ".";
             $uploadOk = 1;
@@ -30,7 +30,7 @@
         $uploadOk = 0;
     }
     // Check file size(kolko e goem)
-    if ($_FILES["files[]"]["size"] > 5000000) {
+    if ($_FILES["imgUpload"]["size"] > 5000000) {
         echo "Sorry, your file is too large.";
         $uploadOk = 0;
     }
@@ -45,8 +45,8 @@
         echo "Sorry, your file was not uploaded.";
     // if everything is ok, try to upload file
     } else {
-        if (move_uploaded_file($_FILES["files[]"]["tmp_name"], $target_file)) {
-            echo "The file ". basename( $_FILES["files[]"]["name"]). " has been uploaded.";
+        if (move_uploaded_file($_FILES["fimgUpload"]["tmp_name"], $target_file)) {
+            echo "The file ". basename( $_FILES["imgUpload"]["name"]). " has been uploaded.";
         } else {
             echo "Sorry, there was an error uploading your file.";
         }
