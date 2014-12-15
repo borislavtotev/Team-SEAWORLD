74d78d4984be553c52a5a2b30614a6f1aacd4dee
Slav Slavchev <slav.slavchev.98@gmail.com>
2014-12-16 01:58:13 +0200

    fix bugsss ....f.f.f.ds.fsd.

--- views/modals/file-upload.php
+++ views/modals/file-upload.php
@@ -1,32 +1,41 @@
 <?php
 include 'views/partials/header.php';
+include 'image-upload.php';
 ?>
-<div class="modal fade" id="filesUpload" tabindex="-1" role="dialog" aria-labelledby="FilesUpload" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-                <h4 class="modal-title" id="myModalLabel">Upload images</h4>
-            </div>
-            <div class="modal-body">
-                <form id="uploadForm" action="#" method="post" class="form-horizontal" enctype="multipart/form-data">
-                    <div class="form-group">
-                        <div class="col-lg-10">
-                            <input type="file" name="files[]" id="files[]">
+<form id="uploadForm" action="#" method="post" class="form-horizontal" enctype="multipart/form-data">
+    <div class="modal fade" id="filesUpload" tabindex="-1" role="dialog" aria-labelledby="FilesUpload" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                    <h4 class="modal-title" id="myModalLabel">Upload images</h4>
+                </div>
+                <div class="modal-body">
+                    <form id="uploadForm" class="form-horizontal">
+                        <div class="form-group">
+                            <div class="col-lg-10">
+                                <input type="file" name="fileToUpload" id="fileToUpload">
+                            </div>
                         </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <div>
+                        <input type="submit" class="btn btn-danger btn-tg pull-left" name="submit"/>
                     </div>
-                </form>
-            </div>
-            <div class="modal-footer">
-                <div>
-                    <button id="add-img-btn" type="button" class="btn btn-default pull-left">Add More Images</button>
+                    <div>
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>                    
                 </div>
-                <div>
-                    <input type="submit" class="btn btn-danger btn-tg" name="submit"/>
-                </div>
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
             </div>
         </div>
     </div>
-</div>
+</form>
 <?php include 'views/partials/footer.php'; ?>
+
+
+<form method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
+</form>
