e68b46fe65147097675f6aa9291651e584f9e95a
asyamincheva <asya.m.mincheva@gmail.com>
2014-12-15 21:31:31 +0200

    added header and footer of about us page

--- about-us.php
+++ about-us.php
@@ -1,1 +1,17 @@
+<?php
+include_once( 'views/partials/header.php' );
+?>
 <!--About us info-->
+    <div class="container">
+        <header class="row">
+            <div class="col-md-12">
+                <div class="jumbotron animated fadeInDownBig">
+                    <hgroup>
+                        <h1 class="text-center">Programators! About us</h1>
+                        <h2 class="text-center">Изпрограмирайте се най - шашаво!</h2>
+                    </hgroup>
+                </div>
+        </header>
+
+    </div>
+<?php include_once( 'views/partials/footer.php' ); ?>
