b775467b0c335a02e967e5538b5c38fffe49bc1d
asyamincheva <asya.m.mincheva@gmail.com>
2014-12-15 22:10:09 +0200

    edited info about us

--- about-us.php
+++ about-us.php
@@ -10,9 +10,19 @@ include_once( 'views/partials/header.php' );
                         <h1 class="text-center">Programators! About us</h1>
                         <h2 class="text-center">Weird programmers. One place.</h2>
                         <h4 class="text-center">
-                            The best place for all the programmers to share what they find funny and interesting to others.
-                            The community will make you feel like this is your second home. Have fun!
+                            <div>
+                                The best place for all the programmers to share what they find funny and interesting to others.
+                            </div>
+                                <ul id="about-info">
+                                    <li>Upload funny photos</li>
+                                    <li>Rate photos</li>
+                                    <li>Comment photos</li>
+                                    <li>Make your own albums with photos</li>
+                                    <li>Be weird - have real Programator fun!</li>
+                                </ul>
                         </h4>
+                        <img src="img/about-us-guy.jpg" alt="" id="baner" class="img-responsive" alt="Responsive image"/>
+                        <div id="about-info">Project created by team Seaworld.</div>
                     </hgroup>
                 </div>
         </header>
