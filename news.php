587d48bdac79bbb950e8dccf578b1d8d0f287d54
Slav Slavchev <slav.slavchev.98@gmail.com>
2014-12-15 23:09:38 +0200

    add news page

---
+++ news.php
@@ -0,0 +1,21 @@
+<?php
+include_once( 'views/partials/header.php' );
+include_once( 'system/db-connect.php' );
+
+
+$albumsCount = 9;
+$rows = ceil( $albumsCount / 3 );
+
+?>
+<main class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <section>
+               
+            </section>
+        </div>
+    </div>
+</main>
+<?php include_once( 'views/partials/footer.php' );
+
+   
