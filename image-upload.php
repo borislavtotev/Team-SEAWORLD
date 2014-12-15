4f4daab6b74ce953c8d8d82399d7e3639e547a25
Bebbo <mrbebz@gmail.com>
2014-12-15 19:12:51 +0200

    change id name

--- image-upload.php
+++ image-upload.php
@@ -45,8 +45,8 @@
         echo "Sorry, your file was not uploaded.";
     // if everything is ok, try to upload file
     } else {
-        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-            echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+        if (move_uploaded_file($_FILES["files[]"]["tmp_name"], $target_file)) {
+            echo "The file ". basename( $_FILES["files[]"]["name"]). " has been uploaded.";
         } else {
             echo "Sorry, there was an error uploading your file.";
         }
