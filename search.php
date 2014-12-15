cc1e06229df77dcf3c69d181d93f558b71ff45a0
d0ntth1nc <d0ntth1nc@gmail.com>
2014-12-15 19:24:33 +0200

    no error when the search input is empty

--- search.php
+++ search.php
@@ -1,10 +1,9 @@
 <?php
-include_once( 'views/partials/header.php' );
-include_once( 'system/db-connect.php' );
-include_once( 'system/search-engine.php' );
-include_once( 'system/models/user.php' );
-
 if (isset( $_GET[ 'pattern' ] ) && !empty( $_GET[ 'pattern' ] )) {
+    include_once( 'views/partials/header.php' );
+    include_once( 'system/db-connect.php' );
+    include_once( 'system/search-engine.php' );
+    include_once( 'system/models/user.php' );
     $results = SearchEngine::searchFor( $mysqli, $_GET[ 'pattern' ] );
 } else {
     header( 'Location: index.php' );
--- system/db-connect.php
+++ system/db-connect.php
@@ -5,7 +5,7 @@
 //$mysql_password = "whatsmyname1";
 
 $slavDb = 'php_teamproject';
-$d0ntth1nc_DBname = 'somedb';
+$d0ntth1nc_DBname = 'test';
 
 $dbName = 'firstDB';
 $dbUser = 'root';
