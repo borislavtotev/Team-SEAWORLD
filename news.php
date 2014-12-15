fe45192776483edd18a85ee7a4e8e17cc95b68b2
Slav Slavchev <slav.slavchev.98@gmail.com>
2014-12-15 23:10:42 +0200

    print pic from folder img at news page

--- news.php
+++ news.php
@@ -11,7 +11,17 @@ $rows = ceil( $albumsCount / 3 );
     <div class="row">
         <div class="col-md-12">
             <section>
-               
+               <?php
+				   $files = glob("img/*.*");
+				   var_dump($files);
+				   for ($i=1; $i<count($files); $i++){
+					  $image = $files[$i];
+
+					  print $image ."<br />";
+					  echo '<img src="'.$image .'" alt="Random image" />'."<br /><br />";
+				   }
+
+				 ?>
             </section>
         </div>
     </div>
