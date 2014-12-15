22ef42f4bc2a13434f98b9a6fb41194decde0ca1
Bebbo <mrbebz@gmail.com>
2014-12-15 19:03:33 +0200

    need some tests
    tests

--- image-upload.php
+++ image-upload.php
@@ -1,8 +1,8 @@
-<?php
-    if((!isset($_SESSION['user']))) {
-        header("Location:index.php");
-}
-?>
+//<?php
+ //   if((!isset($_SESSION['user']))) {
+ //       header("Location:index.php");
+//}
+//?>
 <?php include 'views/modals/file-upload.php'; ?>
 
 <button type="button" class="btn btn-danger btn-lg" data-toggle="modal" data-target="#filesUpload">
