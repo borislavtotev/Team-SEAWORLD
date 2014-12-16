0b3793a1f3a1dbd6230544f6467d4ed7e2578e6a
borislavtotev <borislav.totev@gmail.com>
2014-12-17 01:47:09 +0200

    fix path to ./delete-album.php

--- js/albumsRemover.js
+++ js/albumsRemover.js
@@ -5,7 +5,7 @@ jQuery(function() {
         var albumId = this.dataset.albumid;
         var container = jQuery( e.target.parentNode );
         var button = jQuery(e.target );
-        jQuery.post( '/delete-album.php', { albumId:albumId }, function( result ) {
+        jQuery.post( './delete-album.php', { albumId:albumId }, function( result ) {
             if (!result) {
                 button.remove();
                 container.animate( { width: 0, opacity: 0 }, 1000, function() {
