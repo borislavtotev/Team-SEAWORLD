e1ef92240de0f757fafdd4327d6baec6a8f7d21f
d0ntth1nc <d0ntth1nc@gmail.com>
2014-12-17 20:57:25 +0200

    fix table

--- exported-db-tables/images.sql
+++ exported-db-tables/images.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.10.7
+-- version 4.1.12
 -- http://www.phpmyadmin.net
 --
--- Host: 127.7.75.2:3306
--- Generation Time: Dec 17, 2014 at 03:14 PM
--- Server version: 5.5.40
--- PHP Version: 5.3.3
+-- Host: localhost
+-- Generation Time: Dec 17, 2014 at 07:56 PM
+-- Server version: 5.6.16
+-- PHP Version: 5.5.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `seaworld`
+-- Database: `somedb`
 --
 
 -- --------------------------------------------------------
@@ -30,11 +30,11 @@ CREATE TABLE IF NOT EXISTS `images` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` text COLLATE utf8_unicode_ci NOT NULL,
   `albumid` int(11) NOT NULL,
-  `date-uploaded` int(11) NOT NULL,
+  `date-uploaded` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `votes-up` int(11) NOT NULL DEFAULT '0',
   `votes-down` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=9 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=19 ;
 
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
