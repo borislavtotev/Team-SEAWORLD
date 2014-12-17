ac6e226e63aef6ef79bc1071ad2f6d57290eb9ac
asyamincheva <asya.m.mincheva@gmail.com>
2014-12-17 17:11:42 +0200

    feed - added header, footer, heading

--- feed.php
+++ feed.php
@@ -1,4 +1,15 @@
-<?php
-//This section will replace the one called 'News'
-//Here users will be able to see all photos, rate/comment them
-?>
+<?php include_once( 'views/partials/header.php' ); ?>
+<!--This section will replace the one called 'News'
+    Here users will be able to see all photos, rate/comment them-->
+    <div class="container">
+        <header class="row">
+            <div class="col-md-12">
+                <div class="jumbotron animated fadeInDownBig">
+                    <hgroup>
+                        <h1 class="text-center">Feed</h1>
+                    </hgroup>
+                </div>
+        </header>
+
+    </div>
+<?php include_once( 'views/partials/footer.php' );
