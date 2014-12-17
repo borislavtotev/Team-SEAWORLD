ad4e829c6998f572e96303580657b9e6cf17531b
asyamincheva <asya.m.mincheva@gmail.com>
2014-12-17 17:19:58 +0200

    get URL function added
    added in the header.php in order to use it when you need it; of course,
    when the header is included in the page

--- views/partials/header.php
+++ views/partials/header.php
@@ -12,6 +12,20 @@
 </head>
 <body>
 <?php
+
+//Get current page URL; the method can be called whenever we include the header.php
+function curPageURL() {
+    $pageURL = 'http';
+    if ($_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
+    $pageURL .= "://";
+    if ($_SERVER["SERVER_PORT"] != "80") {
+        $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
+    } else {
+        $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+    }
+    return $pageURL;
+}
+
 function __autoload( $classname )
 {
     $filename = "./system/models/". strtolower( $classname ) .".php";
