946b40ff1bd828a1a6759d310227c7debaa5ed26
asyamincheva <asya.m.mincheva@gmail.com>
2014-12-18 00:25:10 +0200

    edition of feed to include the scroll up btn

--- feed.php
+++ feed.php
@@ -4,26 +4,40 @@
     <!-- Add the icon 'Back to top' button font library -->
     <link rel="stylesheet" id="font-awesome-css" href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css"
           type="text/css" media="screen">
-    <script>
-        $(function(){
-            $(document).on( 'scroll', function(){
-                if ($(window).scrollTop() > 100) {
-                    $('.scroll-top-wrapper').addClass('show');
-                } else {
-                    $('.scroll-top-wrapper').removeClass('show');
-                }
-            });
-        });
-        function scrollToTop() {
-            verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
-            element = $('body');
-            offset = element.offset();
-            offsetTop = offset.top;
-            $('html, body').animate({scrollTop: offsetTop}, 500, 'linear');
-        }
-    </script>
+
+
 </head>
 <body>
+<script>
+    $(function(){
+        $(document).on( 'scroll', function(){
+
+            if ($(window).scrollTop() > 100) {
+                $('.scroll-top-wrapper').addClass('show');
+            } else {
+                $('.scroll-top-wrapper').removeClass('show');
+            }
+        });
+
+        $('.scroll-top-wrapper').on('click', scrollToTop);
+    });
+
+    function scrollToTop() {
+        verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
+        element = $('body');
+        offset = element.offset();
+        offsetTop = offset.top;
+        $('html, body').animate({scrollTop: offsetTop}, 500, 'linear');
+    }
+</script>
+
+<!-- Add div for the icon 'Back to top' button -->
+<div class="scroll-top-wrapper ">
+	<span class="scroll-top-inner">
+		<i class="fa fa-2x fa-arrow-circle-up"></i>
+	</span>
+</div>
+
 <!-- Add jQuery to use it for the icon 'Back to top' button -->
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 
@@ -37,14 +51,18 @@
                 <hgroup>
                     <h1 class="text-center">Feed</h1>
                 </hgroup>
+                <?php
+                for($i=9; $i<500; $i++){
+                    ?>
+                    <div class="tryButton">
+                        Try to see if works
+                    </div> <?php
+                }
+                ?>
             </div>
+
+
     </header>
-</div>
-<!-- Add div for the icon 'Back to top' button -->
-<div class="scroll-top-wrapper ">
-	<span class="scroll-top-inner">
-		<i class="fa fa-2x fa-arrow-circle-up"></i>
-	</span>
 </div>
 <?php include_once( 'views/partials/footer.php' );
 ?>
