0c64bc255d258c86086d49a0feaa23154f2e48ec
d0ntth1nc <d0ntth1nc@gmail.com>
2014-12-19 02:18:43 +0200

    wrap long text

--- system/db-connect.php
+++ system/db-connect.php
@@ -12,7 +12,7 @@ $dbUser = 'root';
 $dbPass = '';
 $dbLocation = 'localhost';
 
-$mysqli = mysqli_connect( 'localhost', 'root', '', $asyaDBname );
+$mysqli = mysqli_connect( 'localhost', 'root', '', $d0ntth1nc_DBname );
 
 if ($mysqli->connect_errno) {
     die( "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error );
